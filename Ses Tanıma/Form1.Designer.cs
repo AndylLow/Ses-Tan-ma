3fd1553a7faafd1332e668bc3289065ea3c64cca
AndylLow <39218242+AndylLow@users.noreply.github.com>
2019-07-01 10:47:42 +0300

    Initial

--- Ses Tanıma/Form1.Designer.cs
+++ Ses Tanıma/Form1.Designer.cs
@@ -28,13 +28,65 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBoxPause = new System.Windows.Forms.PictureBox();
+            this.pictureBoxMic = new System.Windows.Forms.PictureBox();
+            this.textBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPause)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxPause
+            // 
+            this.pictureBoxPause.Image = global::Ses_Tanıma.Properties.Resources.Pause_Normal_Red_icon;
+            this.pictureBoxPause.Location = new System.Drawing.Point(12, 12);
+            this.pictureBoxPause.Name = "pictureBoxPause";
+            this.pictureBoxPause.Size = new System.Drawing.Size(72, 72);
+            this.pictureBoxPause.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxPause.TabIndex = 1;
+            this.pictureBoxPause.TabStop = false;
+            // 
+            // pictureBoxMic
+            // 
+            this.pictureBoxMic.Image = global::Ses_Tanıma.Properties.Resources.Microphone_icon1;
+            this.pictureBoxMic.Location = new System.Drawing.Point(12, 12);
+            this.pictureBoxMic.Name = "pictureBoxMic";
+            this.pictureBoxMic.Size = new System.Drawing.Size(72, 72);
+            this.pictureBoxMic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxMic.TabIndex = 0;
+            this.pictureBoxMic.TabStop = false;
+            this.pictureBoxMic.Click += new System.EventHandler(this.pictureBoxMic_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(204, 12);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(584, 426);
+            this.textBox.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.pictureBoxMic);
+            this.Controls.Add(this.pictureBoxPause);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPause)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxMic;
+        private System.Windows.Forms.PictureBox pictureBoxPause;
+        private System.Windows.Forms.TextBox textBox;
     }
 }
 
--- Ses Tanıma/Form1.cs
+++ Ses Tanıma/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Speech.Recognition;
 
 namespace Ses_Tanıma
 {
@@ -15,6 +16,34 @@ namespace Ses_Tanıma
         public Form1()
         {
             InitializeComponent();
+        }
+        public SpeechRecognitionEngine recognitionEngine = new SpeechRecognitionEngine();
+        private void pictureBoxMic_Click(object sender, EventArgs e)
+        {
+            pictureBoxMic.Visible = false;
+            pictureBoxPause.Visible = true;
+            recognitionEngine.RecognizeAsync();
+        }
+
+        private void pictureBoxPause_Click(object sender, EventArgs e)
+        {
+            pictureBoxPause.Visible = false;
+            pictureBoxMic.Visible = true;
+            recognitionEngine.RecognizeAsyncCancel();
+        }
+        public void sestanima_ayarlar()
+        {
+            string[] ihtimaller = { "merhaba", "Dünya" };
+            Choices seçenekler = new Choices(ihtimaller);
+            Grammar grammar = new Grammar(new GrammarBuilder(seçenekler));
+            recognitionEngine.LoadGrammar(grammar);
+            recognitionEngine.SetInputToDefaultAudioDevice();
+            recognitionEngine.SpeechRecognized += ses_tanindiginda;
+        }
+
+        private void ses_tanindiginda(object sender, SpeechRecognizedEventArgs e)
+        {
+            textBox.Text = textBox.Text+" "+e.Result.Text;
         }
     }
 }
